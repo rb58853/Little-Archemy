fdf228b47f9ccd45b01f45d908bafd668b1a9f0a
Raúl Beltrán <rb58853@gmail.com>
2022-11-14 09:17:12 +0100

    new config in django

--- little_archemy_backend/little_archemy/settings.py
+++ little_archemy_backend/little_archemy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=*y9@kkx6852=31mb8wxun(d=9)x63hsy6kmu0_1a_(%-ekcji
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
