a10130b79456a3dbf0cd4264da766de6d7e544e0
Raúl Beltrán <114294772+rb58853@users.noreply.github.com>
2022-12-06 16:51:42 -0500

    Fixed index range exeption, in create basic

--- telegram_bot_fronted/admin.py
+++ telegram_bot_fronted/admin.py
@@ -16,7 +16,7 @@ def create_basic (message):
         name = ""
         for i in range(1,len(data_text)-1):
             name += data_text[i]+" "
-        name += data_text[len(data_text)]
+        name += data_text[len(data_text)-1]
         reply = post_create_basic(name)
 
         bot.send_message(id,reply,parse_mode="html")
